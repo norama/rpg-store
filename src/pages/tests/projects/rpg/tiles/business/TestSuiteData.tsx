d9960ef2c235fb2d037a70e2ba4f09df23ea9c93
Nora Mate <norama.matema@gmail.com>
2022-11-18 14:35:37 +0100

    Tests: cleanup

--- src/pages/tests/projects/rpg/tiles/business/TestSuiteData.tsx
+++
@@ -1,6 +0,0 @@
-import TestSuite from 'tests/components/TestSuite'
-import data from 'tests/projects/rpg/tiles/business/data'
-
-const TestSuiteTiles = () => <TestSuite suite={data} />
-
-export default TestSuiteTiles
--- src/pages/tests/projects/rpg/tiles/stores/TestSuiteTiles.tsx
+++
@@ -1,6 +0,0 @@
-import TestSuite from 'tests/components/TestSuite'
-import tiles from 'tests/projects/rpg/tiles/stores/tiles'
-
-const TestSuiteTiles = () => <TestSuite suite={tiles} />
-
-export default TestSuiteTiles
