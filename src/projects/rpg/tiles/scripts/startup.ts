82a4539a2eb2a7bb6ce3ea2da8be550c1f20dc86
Nora Mate <norama.matema@gmail.com>
2022-11-23 15:38:53 +0100

    test 2

--- src/projects/rpg/tiles/scripts/startup.ts
+++ src/projects/rpg/tiles/scripts/startup.ts
@@ -1,10 +1,10 @@
-if (import.meta.env.PUBLIC_LOGGING === 'true') {
-  await import('pubsub/log')
-}
+//if (import.meta.env.PUBLIC_LOGGING === 'true') {
+await import('pubsub/log')
+//}
 
-if (import.meta.env.PUBLIC_LOCAL_STORAGE === 'true') {
-  await import('projects/rpg/tiles/business/localStorageData')
-}
+//if (import.meta.env.PUBLIC_LOCAL_STORAGE === 'true') {
+await import('projects/rpg/tiles/business/localStorageData')
+//}
 
 import data from 'projects/rpg/tiles/business/data'
 await data.initClient()
