f9e6d344bd311fc5e8792fcae1ef3eeabcddfe5a
Nora Mate <norama.matema@gmail.com>
2022-11-26 02:06:36 +0100

    env back

--- src/projects/rpg/db/supabase.ts
+++ src/projects/rpg/db/supabase.ts
@@ -13,14 +13,10 @@ class Database {
 
   init() {
     if (!this.db) {
-      const DATABASE_URL = process.env.DATABASE_URL ?? import.meta.env.DATABASE_URL
-      const SUPABASE_SERVICE_API_KEY =
-        process.env.SUPABASE_SERVICE_API_KEY ?? import.meta.env.SUPABASE_SERVICE_API_KEY
+      const { DATABASE_URL, SUPABASE_SERVICE_API_KEY } = import.meta.env
+      console.log('DATABASE_URL', DATABASE_URL)
 
-      this.db = createClient(
-        'https://fwquxyatrztkypzlvnmp.supabase.co',
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ3cXV4eWF0cnp0a3lwemx2bm1wIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY2OTM5ODA2NywiZXhwIjoxOTg0OTc0MDY3fQ.S2NQpdIsZLfKJcpw3zlvQHQTJK7jlT7m9jdxvp5Z9sY'
-      )
+      this.db = createClient(DATABASE_URL, SUPABASE_SERVICE_API_KEY)
     }
   }
 
