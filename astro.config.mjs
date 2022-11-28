f49235451f7b0af661d229651bcc8119eea441e6
Nora Mate <norama.matema@gmail.com>
2022-11-28 22:42:14 +0100

    added mdx support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import solidJs from '@astrojs/solid-js'
+import solidJs from '@astrojs/solid-js';
 
 // https://astro.build/config
-import netlify from '@astrojs/netlify/functions'
+import netlify from '@astrojs/netlify/functions';
+
+// https://astro.build/config
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()],
+  integrations: [solidJs(), mdx()],
   output: 'server',
-  adapter: netlify(),
-})
+  adapter: netlify()
+});
