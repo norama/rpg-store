15edee73dd6e8817a55963f9d1c537880875f2f5
Nora Mate <norama.matema@gmail.com>
2022-12-20 23:51:14 +0100

    reexport propertiesMap, targetAtom from store

--- src/projects/rpg/builder/business/blocks/advantages/store.ts
+++ src/projects/rpg/builder/business/blocks/advantages/store.ts
@@ -1,5 +1,7 @@
 import { FormData, ResourceAtom } from '@builder/ui/stores/businessStore'
 import { propertiesMap } from '@builder/business/store/properties'
+import { targetAtom } from '@builder/business/store/target'
+
 import PubSub from 'pubsub-js'
 import M from 'pubsub/messages'
 
@@ -14,3 +16,5 @@ export const infoAtom = blockInfo.atom
 PubSub.subscribe(M.uiSaveAction, () => {
   PubSub.publish(M.uiSave, { block: blockMap.get(), properties: propertiesMap.get() })
 })
+
+export { propertiesMap, targetAtom }
--- src/projects/rpg/builder/business/blocks/races/store.ts
+++ src/projects/rpg/builder/business/blocks/races/store.ts
@@ -1,5 +1,6 @@
 import { FormData, ResourceAtom } from '@builder/ui/stores/businessStore'
 import { propertiesMap } from '@builder/business/store/properties'
+import { targetAtom } from '@builder/business/store/target'
 import PubSub from 'pubsub-js'
 import M from 'pubsub/messages'
 
@@ -14,3 +15,5 @@ export const infoAtom = blockInfo.atom
 PubSub.subscribe(M.uiSaveAction, () => {
   PubSub.publish(M.uiSave, { block: blockMap.get(), properties: propertiesMap.get() })
 })
+
+export { propertiesMap, targetAtom }
