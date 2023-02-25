a2696b45dc15412baab9aa4d93c358d05631e616
Nora Mate <norama.matema@gmail.com>
2023-02-25 16:16:58 +0100

    races added info columns

--- src/projects/rpg/builder/business/@types/races.d.ts
+++ src/projects/rpg/builder/business/@types/races.d.ts
@@ -3,6 +3,16 @@ type IRace = {
   points: number
   strength: number
   wiki: string
+  agility: number
+  skill: number
+  physicalEndurance: number
+  mentalEndurance: number
+  IQ: number
+  wisdom: number
+  conduct: number
+  beauty: number
+  HP: number
+  lifeExpectancy: number
 }
 
 type IInfoRaces = Record<string, IRace>
