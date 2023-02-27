fb36f6448a8a585660b6d9976ea9ee6c01cc585d
Nora Mate <norama.matema@gmail.com>
2023-02-27 21:46:47 +0100

    FormControl show when ready

--- src/projects/rpg/builder/ui/widgets/form/FormControls.tsx
+++ src/projects/rpg/builder/ui/widgets/form/FormControls.tsx
@@ -2,14 +2,21 @@ import ResetButton from '@builder/ui/widgets/form/ResetButton'
 import SaveButton from '@builder/ui/widgets/form/SaveButton'
 import { Stack } from '@suid/material'
 import { Toaster } from 'solid-toast'
+import { Show } from 'solid-js'
+import { useStore } from '@nanostores/solid'
+import readyAtom from '@builder/ui/stores/readyAtom'
 
-const FormControls = () => (
-  <>
-    <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={2}>
-      <SaveButton /> <ResetButton />
-    </Stack>
-    <Toaster position="bottom-right" toastOptions={{ duration: 5000 }} />
-  </>
-)
+const FormControls = () => {
+  const ready = useStore(readyAtom)
+
+  return (
+    <Show when={ready()}>
+      <Stack direction="row" justifyContent="flex-end" alignItems="center" spacing={2}>
+        <SaveButton /> <ResetButton />
+      </Stack>
+      <Toaster position="bottom-right" toastOptions={{ duration: 5000 }} />
+    </Show>
+  )
+}
 
 export default FormControls
