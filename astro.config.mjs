68190914d3ebd915a50490ad95a4f8a939dfc03c
Nora Mate <norama.matema@gmail.com>
2023-02-28 20:22:18 +0100

    vercel edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,15 +11,13 @@ import mdx from '@astrojs/mdx'
 import suidPlugin from '@suid/vite-plugin'
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/edge'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), mdx()],
   output: 'server',
-  adapter: vercel({
-    includeFiles: ['./src/pages/api/rpg/builder/api/properties.json'],
-  }),
+  adapter: vercel(),
   vite: {
     plugins: [suidPlugin()],
     build: {
