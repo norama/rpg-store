0026eef03da2460db1615d7e22ccc98674747a0d
Nora Mate <norama.matema@gmail.com>
2023-02-28 20:13:24 +0100

    vercel config includeFiles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,29 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-import solidJs from '@astrojs/solid-js';
+import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
-import mdx from '@astrojs/mdx';
-import suidPlugin from '@suid/vite-plugin';
+import mdx from '@astrojs/mdx'
+import suidPlugin from '@suid/vite-plugin'
 
 // https://astro.build/config
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs(), mdx()],
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    includeFiles: ['./src/pages/api/rpg/builder/api/properties.json'],
+  }),
   vite: {
     plugins: [suidPlugin()],
     build: {
-      target: 'esnext'
-    }
-  }
-});
+      target: 'esnext',
+    },
+  },
+})
