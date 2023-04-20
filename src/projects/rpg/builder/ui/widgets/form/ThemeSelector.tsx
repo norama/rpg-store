847a029fe76fdb8fe19621079511802ab9c6eab2
Nora Mate <norama.matema@gmail.com>
2023-04-20 21:18:36 +0200

    theme cleanup

--- src/projects/rpg/builder/ui/widgets/form/ThemeSelector.tsx
+++ src/projects/rpg/builder/ui/widgets/form/ThemeSelector.tsx
@@ -2,12 +2,9 @@ import PubSub from 'pubsub-js'
 import M from 'pubsub/messages'
 import themeHolder, { ITheme } from 'styles/theme'
 import { useStore } from '@nanostores/solid'
-import { persistentAtom } from '@nanostores/persistent'
-import { Show, onMount } from 'solid-js'
+import { Show } from 'solid-js'
 import SingleSelect from '@builder/ui/widgets/form/input/SingleSelect'
 import style from 'styles/style'
-
-const themeInProgressAtom = persistentAtom<string>('themeInProgress', '0')
 
 const themes = [
   { value: 'light', label: 'světlý' },
@@ -22,17 +19,6 @@ const themeMap = themes.reduce((acc, curr) => {
 const ThemeSelector = () => {
   const theme = useStore(themeHolder.atom)
 
-  onMount(() => {
-    const progress = themeInProgressAtom.get()
-    if (progress === '1') {
-      themeInProgressAtom.set('0')
-      //location.reload()
-    } else if (progress === '2') {
-      themeInProgressAtom.set('0')
-      location.reload()
-    }
-  })
-
   return (
     <Show when={theme()}>
       <div style={{ transform: 'scale(0.8)' }}>
@@ -40,7 +26,6 @@ const ThemeSelector = () => {
           onChange={(value) => {
             if (value !== theme()) {
               PubSub.subscribeOnce(M.uiThemeStored, () => {
-                themeInProgressAtom.set('1')
                 location.reload()
               })
               PubSub.publish(M.uiThemeChanged, value as ITheme)
