281247f4f441b17cbc156b76fbb091bdb86ac560
Nora Mate <norama.matema@gmail.com>
2023-04-24 00:24:25 +0200

    Show in CardLink

--- src/projects/rpg/builder/ui/widgets/CardLink.tsx
+++ src/projects/rpg/builder/ui/widgets/CardLink.tsx
@@ -1,7 +1,8 @@
 import { Button, Link } from '@kobalte/core'
-import { JSXElement } from 'solid-js'
+import { JSXElement, Show } from 'solid-js'
 import themeHolder from 'styles/theme'
 import { useStore } from '@nanostores/solid'
+import readyAtom from '@builder/ui/stores/readyAtom'
 import { createMediaQuery } from '@solid-primitives/media'
 import './CardLink.scss'
 
@@ -16,32 +17,35 @@ type Props = {
 const CardLink = ({ header, action, href, customStyle, children }: Props) => {
   const isSmall = createMediaQuery('(max-width: 70rem)')
   const themeObject = useStore(themeHolder.theme)
+  const ready = useStore(readyAtom)
 
   return (
-    <div
-      style={{
-        color: themeObject()?.colors['sharp'],
-        'background-color': themeObject()?.colors['card'],
-        ...customStyle,
-      }}
-      class={`card ${isSmall() ? 'small' : 'large'}`}
-    >
-      <h2 style={{ color: themeObject()?.colors['background'] }}>{header}</h2>
+    <Show when={ready()}>
+      <div
+        style={{
+          color: themeObject()?.colors['sharp'],
+          'background-color': themeObject()?.colors['card'],
+          ...customStyle,
+        }}
+        class={`card ${isSmall() ? 'small' : 'large'}`}
+      >
+        <h2 style={{ color: themeObject()?.colors['background'] }}>{header}</h2>
 
-      <div>{children}</div>
-      <div class="cardAction">
-        <Button.Root
-          style={{
-            color: themeObject()?.colors['linkButton'],
-            'border-color': themeObject()?.colors['muted'],
-            background: 'transparent',
-          }}
-          class="LinkButton cardAction"
-        >
-          <Link.Root href={href}>{action}</Link.Root>
-        </Button.Root>
+        <div>{children}</div>
+        <div class="cardAction">
+          <Button.Root
+            style={{
+              color: themeObject()?.colors['linkButton'],
+              'border-color': themeObject()?.colors['muted'],
+              background: 'transparent',
+            }}
+            class="LinkButton cardAction"
+          >
+            <Link.Root href={href}>{action}</Link.Root>
+          </Button.Root>
+        </div>
       </div>
-    </div>
+    </Show>
   )
 }
 
