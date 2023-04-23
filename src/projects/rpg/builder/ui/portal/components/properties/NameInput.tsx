853802f1568f4421ed64f751c672fe35df927d80
Nora Mate <norama.matema@gmail.com>
2023-04-24 00:12:20 +0200

    Show in NameInput

--- src/projects/rpg/builder/ui/portal/components/properties/NameInput.tsx
+++ src/projects/rpg/builder/ui/portal/components/properties/NameInput.tsx
@@ -1,21 +1,26 @@
 import { propertiesMap } from '@builder/business/store/properties'
 import StringInput from '@input/StringInput'
+import { Show } from 'solid-js'
 import { useStore } from '@nanostores/solid'
 import stateAtom from '@builder/ui/stores/stateAtom'
+import readyAtom from '@builder/ui/stores/readyAtom'
 
 const NameInput = () => {
   const properties = useStore(propertiesMap)
   const state = useStore(stateAtom)
+  const ready = useStore(readyAtom)
 
   return (
-    <StringInput
-      label="Jméno"
-      value={() => properties().name}
-      disabled={state() === 'saving'}
-      onChange={(name) => propertiesMap.setKey('name', name)}
-      placeholder="Zadejte název postavy"
-      customStyle={{ width: '100%' }}
-    />
+    <Show when={ready()}>
+      <StringInput
+        label="Jméno"
+        value={() => properties().name}
+        disabled={state() === 'saving'}
+        onChange={(name) => propertiesMap.setKey('name', name)}
+        placeholder="Zadejte název postavy"
+        customStyle={{ width: '100%' }}
+      />
+    </Show>
   )
 }
 
